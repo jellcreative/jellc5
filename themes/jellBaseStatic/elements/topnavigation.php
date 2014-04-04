5e7718f8dd64c38ccc1ec3f13293bbbf7181cb74
John Czaplicki <j@jec3.me>
2014-04-04 12:09:38 -0500

    jell mods

---
+++ themes/jellBaseStatic/elements/topnavigation.php
@@ -0,0 +1,22 @@
+<header>
+     <div class="width_holder" >
+        <img id="printlogo" src="<?=$this->getThemePath()?>/images/jell_logo.png" /> <a id="logo" title="Home" href="/"><img src="<?=$this->getThemePath()?>/images/jell_logo.svg" alt="" /></a>
+
+
+        <!-- <nav> -->
+        <?
+        $top = BlockType::getByHandle('autonav');       
+        $top->controller->orderBy = 'display_asc';                    
+        $top->controller->displayPages = 'top';
+        $top->controller->displaySubPages = 'all';     
+        $top->controller->displaySubPageLevels = 'custom';
+        $top->controller->displaySubPageLevelsNum = '2';     
+        $top->render('templates/main_nav');
+        ?>
+        <!-- end <nav> -->
+    
+    </div><!--end width_holder-->
+    <!-- google analytics -->
+
+    <!-- end google analytics -->
+</header>
