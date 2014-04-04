eb90c34bf0d54887627648dc8fbf1951ee570107
John Czaplicki <j@jec3.me>
2014-04-04 15:53:58 -0500

    polish styling

--- themes/jell_base_static/default.php
+++ themes/jell_base_static/default.php
@@ -2,8 +2,8 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 $this->inc('elements/header.php'); ?>
 <div id="content_inner">
-<?php $a = new Area('Main');
-$a->display($c); ?>
+	<?php $a = new Area('Main');
+	$a->display($c); ?>
 </div>
 <? $this->inc('elements/footer.php');
 ?>
